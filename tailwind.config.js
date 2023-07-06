dc8bee66fca3d4158f91e8394efa3acf142ad27e
Mikynek <mikysek.jakub@gmail.com>
2023-07-06 19:28:53 +0200

    new: finished header (buttons, info, etc.)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'red-orange': '#ff4000',
+      'red-orange-light': '#ff4f14',
+      'red-orange-dark': '#eb3b00',
+      'white': '#ffffff',
+      'gray-white': '#f5f5f5'
+
+    },
     extend: {},
   },
   plugins: [
