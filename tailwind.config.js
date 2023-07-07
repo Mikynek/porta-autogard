c83fee8650183d951c05d72afc837a58b8365ded
Mikynek <mikysek.jakub@gmail.com>
2023-07-07 12:46:44 +0200

    new: section after header

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,10 @@ module.exports = {
       'red-orange': '#ff4000',
       'red-orange-light': '#ff4f14',
       'red-orange-dark': '#eb3b00',
+      'blue': '#0f4c64',
       'white': '#ffffff',
+      'gray': '#e0e4e3',
       'gray-white': '#f5f5f5'
-
     },
     extend: {},
   },
